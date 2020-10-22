39468125080ce8c08ee52b66569a8aeaa35fbfbd
Jasmine Awatramani <42552965+jazzy28@users.noreply.github.com>
2020-10-23 04:02:20 +0530

    Update and rename 01-insertAtStart.c to 01-singleLL.c

---
+++ 03-Linked List/01-singleLL.c
@@ -0,0 +1,13 @@
+//not sequential structure.
+//may take longer time
+//data and struct node *link : pointing to a same type of structure
+//single link: moves forward only
+//arrays cannot be created dynamically, but using structures it can be done. add an element and connect it with link
+#include <stdio.h>
+
+struct node{
+    char data;
+    struct node *link;
+};
+
+
