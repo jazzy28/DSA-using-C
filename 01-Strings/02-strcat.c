966eab74b57031f0a7d236157708ffe5dd143f29
Jasmine Awatramani <42552965+jazzy28@users.noreply.github.com>
2020-10-22 23:21:40 +0530

    Create 02-strcat.c

---
+++ 01-Strings/02-strcat.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void strcat(char s[], char t[]){ //char *s = char s[]
+    int i = 0, j=0;
+    while((s[i] != '\0')
+        i++;
+    while((s[i++] == t[j++]) != '\0');
+    
+}
