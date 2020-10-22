da687554992c15cb17f5311fc789009951799e6f
Jasmine Awatramani <42552965+jazzy28@users.noreply.github.com>
2020-10-22 23:43:58 +0530

    Update 06-numToString.c

--- 01-Strings/06-numToString.c
+++ 01-Strings/06-numToString.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+
+
+void itoa(int n, char s[]){ 
+    int i, sign; //sign: positive or negative number
+    if((sign = n) < 0)
+        n = -n;
+    
+    i = 0;
+    do{
+        s[i++] = n%10 + '0'; //'0' show will ascii value of 0(48) then we can add any number to find out their ascii value
+    }while((n/=10)>0);
+    
+    if(sign<0)
+        s[i++] = '-';
+    s[i] = '\0';
+    reverse(s);
+}
+  
