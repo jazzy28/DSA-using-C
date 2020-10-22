b9fea286f04256a92659d31fe6a854609c659b8b
Jasmine Awatramani <42552965+jazzy28@users.noreply.github.com>
2020-10-23 04:14:31 +0530

    Update 02-traverseSingleLL.c

--- 03-Linked List/02-traverseSingleLL.c
+++ 03-Linked List/02-traverseSingleLL.c
@@ -1,1 +1,18 @@
+//last node will contain null
 
+#include <stdio.h>
+
+struct node{
+    int i;
+    struct node *link;
+};
+
+struct node *head;
+struct node *temp;
+
+temp = head; //temp pointer, so that head isn't lost
+
+while(t != NULL){
+printf("%d", temp->i);
+temp = temp -> link;
+}
