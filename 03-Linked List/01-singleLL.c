fc05b5ce1db6007b23cbb80841e064a85eca2918
Jasmine Awatramani <42552965+jazzy28@users.noreply.github.com>
2020-10-23 04:09:05 +0530

    Update 01-singleLL.c

--- 03-Linked List/01-singleLL.c
+++ 03-Linked List/01-singleLL.c
@@ -3,6 +3,7 @@
 //data and struct node *link : pointing to a same type of structure
 //single link: moves forward only
 //arrays cannot be created dynamically, but using structures it can be done. add an element and connect it with link
+
 #include <stdio.h>
 
 struct node{
@@ -10,4 +11,9 @@ struct node{
     struct node *link;
 };
 
+struct node *head;
+struct node *p;
 
+p = head -> link -> link; //data inside the second node
+
+printf("%c", head -> link -> link -> link -> link -> data);
