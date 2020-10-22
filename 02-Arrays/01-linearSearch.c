39f2a8f3ea00a1f12d2cbc8427e3d1c6c03a1f59
Jasmine Awatramani <42552965+jazzy28@users.noreply.github.com>
2020-10-23 03:24:51 +0530

    Update 01-linearSearch.c

--- 02-Arrays/01-linearSearch.c
+++ 02-Arrays/01-linearSearch.c
@@ -1,3 +1,13 @@
 //sequential search
 //unsorted and sorted data both
 //index: 0 to n-1
+
+#include <stdio.h>
+
+int linearSearch(int *arr, int size, int target){
+    for(int i = 0; i<size; i++){
+        if(arr[i] == target)
+            return i;
+        return -1;
+    }
+}
